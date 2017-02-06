8a02fcc6057b9b39c36c669714f0dd983212c365
ToF <cthibaut@octo.com>
2017-02-06 16:43:57 +0100

    Frames: done

--- Frames/Bowling.hs
+++ Frames/Bowling.hs
@@ -5,18 +5,22 @@ import Data.Maybe
 type Throw = Int
 type Score = Int
 type Frame = (Int,Result)
+type Index = Int
 
 data Result = Normal | Half Int | Spare | Strike
     deriving (Eq,Show)
 
 score :: [Throw] -> Score
-score ts = points ts $ marks $ frames $ results ts
+score ts = collect ts $ marks $ frames $ results ts
     where 
-    points :: [Throw] -> [Int] -> Score
-    points ts ps = sum $ map (fromJust . flip lookup game) ps
+    collect :: [Throw] -> [Index] -> Score
+    collect ts ps = sum $ map throwAt ps
         where
         game :: [(Int,Throw)]
         game = zip [0..] ts
+
+        throwAt :: Index -> Throw
+        throwAt = fromJust . flip lookup game
 
 results :: [Throw] -> [Result]
 results = tail . scanl result Normal 
@@ -28,15 +32,14 @@ frames = tail . scanl frame (-1,Normal)
     frame (f,Half _) r   = (f, r)
     frame (f,_)      r   = (succ f, r)
 
-marks :: [Frame] -> [Int]
+marks :: [Frame] -> [Index]
 marks fs = concat $ zipWith mark fs [0..]
     where
-    mark :: Frame -> Int -> [Int]
+    mark :: Frame -> Index -> [Index]
     mark (f,_) _ | f == 10 = []
-    mark (_,Normal) t   = [t]
-    mark (_,Half _) t   = [t]
-    mark (_,Spare)  t   = [t, t+1]
-    mark (_,Strike) t   = [t, t+1, t+2]
+    mark (_,Spare)  i   = [i, i+1]
+    mark (_,Strike) i   = [i, i+1, i+2]
+    mark (_,_)      i   = [i]
     
 result :: Result -> Throw -> Result
 result (Half n) t | n + t == 10 = Spare
