2cbcf1ea729ee36b6d1a0ae73d64b5737a89ac97
ToF <cthibaut@octo.com>
2017-02-06 13:43:24 +0100

    Gamestate: games with spares

--- GameState/Bowling.hs
+++ GameState/Bowling.hs
@@ -1,17 +1,34 @@
 module Bowling
 where
 
-type GameState = Int
+data GameState = GameState { _Score :: Score,
+                             _Bonus :: Bonus,
+                             _Last  :: Maybe Throw}
 type Throw = Int
+type Score = Int
+type Bonus = Bool
 
-score :: [Throw] -> Int
-score = getScore . foldl add initial 
+score :: [Throw] -> Score
+score = _Score . foldl add initial 
     where
     initial :: GameState 
-    initial = 0
+    initial = GameState { _Score = 0, _Bonus = False, _Last = Nothing }
 
     add :: GameState -> Throw -> GameState
-    add = (+)
+    add (GameState sc b l) t = GameState { _Score = newScore sc b t, 
+                                           _Bonus = newBonus l t,
+                                           _Last  = newLast l t}
+        where 
+        newScore :: Score -> Bonus -> Throw -> Score
+        newScore sc False t = sc + t 
+        newScore sc True  t = sc + t + t
 
-    getScore :: GameState -> Int
-    getScore = id
+        newBonus :: Maybe Throw -> Throw -> Bonus
+        newBonus l t = fmap (+t) l == Just 10   
+
+        newLast :: Maybe Throw -> Throw -> Maybe Throw
+        newLast (Just _) _ = Nothing
+        newLast Nothing  t = Just t
+    
+
+
--- GameState/Specs.hs
+++ GameState/Specs.hs
@@ -7,3 +7,6 @@ main = hspec $ do
             it "for games with no special points" $ do
                 score [3,6,6,3,5,4,4,5,1,8,8,1,9,0,0,9,2,7,7,2] `shouldBe` 90
                 score [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] `shouldBe` 0
+            it "for games with spares" $ do
+                score [5,5,5,5] `shouldBe` 25
+                score [8,2,1,9,4,4] `shouldBe` 33
