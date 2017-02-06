d5a8509bf735a560e2b41e6c5b85e10b231e2d04
ToF <cthibaut@octo.com>
2017-02-06 12:53:30 +0100

    recursive:games with strikes

--- Recursive/Bowling.hs
+++ Recursive/Bowling.hs
@@ -5,6 +5,7 @@ type Throw = Int
 
 score :: [Throw] -> Int
 score [] = 0
+score (x:y:z:ts) | x == 10   = x+y+z + score (y:z:ts)
 score (x:y:z:ts) | x+y == 10 = x+y+z + score (z:ts)
                  | otherwise = x+y   + score (z:ts)
 score (x:ts)                 = x     + score ts
