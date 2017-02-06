dab3858333bec68d639c32a667ad6f2363e2950e
ToF <cthibaut@octo.com>
2017-02-06 14:45:54 +0100

    Gamestate: done

--- GameState/Bowling.hs
+++ GameState/Bowling.hs
@@ -1,48 +1,50 @@
 module Bowling
 where
 
-data GameState = GameState { _Score :: Score,
-                             _Bonus :: Bonus,
-                             _Last  :: Maybe Throw,
-                             _Frame :: Frame}
+data GameState = GameState { _score :: Score,
+                             _bonus :: Bonus,
+                             _last  :: Last,
+                             _frame :: Frame}
 
     deriving (Eq,Show)
 
 type Throw = Int
 type Score = Int
 type Bonus = (Int,Int)
+type Last  = Maybe Throw
 type Frame = Int
 
 score :: [Throw] -> Score
-score = _Score . foldl add initial 
+score = _score . foldl add initial 
 
 add :: GameState -> Throw -> GameState
-add (GameState sc b l f) t = GameState { _Score = newScore sc b t f, 
-                                         _Bonus = newBonus b l t f,
-                                         _Last  = newLast l t,
-                                         _Frame = newFrame f l t}
+add g@(GameState sc b l f) t = GameState { _score = newScore g t,
+                                           _bonus = newBonus g t,
+                                           _last  = newLast l t,
+                                           _frame = newFrame f l t}
     where 
-    newScore :: Score -> Bonus -> Throw -> Frame -> Score
-    newScore sc (n,_) t 10 = sc + t 
-    newScore sc (n,_) t f  = sc + t + t * n
+    newScore :: GameState -> Throw -> Score
+    newScore (GameState sc (n,_) _ 10) t = sc + 0 + (t * n) 
+    newScore (GameState sc (n,_) _ _) t = sc + t + (t * n)
 
-    newBonus :: Bonus -> Maybe Throw -> Throw -> Frame -> Bonus
-    newBonus (_,b) _ _ 10                        = (b,0)
-    newBonus (_,b) Nothing 10 _                  = (b+1,1)
-    newBonus (_,b) l t _ | fmap (+t) l == Just 10 = (1,0)  
-                         | otherwise              = (b,0)
+    newBonus :: GameState -> Throw -> Bonus
+    newBonus (GameState _ (_,b) _      10) _  = (b,0)
+    newBonus (GameState _ (_,b) Nothing _) 10 = (b+1,1)
+    newBonus (GameState _ (_,b) l       _) t | fmap (+t) l == Just 10 = (1,0)
+                                             | otherwise              = (b,0)
 
-    newLast :: Maybe Throw -> Throw -> Maybe Throw
+    newLast :: Last -> Throw -> Last
     newLast Nothing 10 = Nothing
     newLast (Just _) _ = Nothing
     newLast Nothing  t = Just t
 
-    newFrame :: Frame -> Maybe Throw -> Throw -> Frame
+    newFrame :: Frame -> Last -> Throw -> Frame
+    newFrame 10 _       _  = 10
     newFrame f (Just _) _  = succ f
     newFrame f Nothing  10 = succ f
     newFrame f Nothing  _  = f
     
 initial :: GameState 
-initial = GameState { _Score = 0, _Bonus = (0,0), _Last = Nothing, _Frame = 0 }
+initial = GameState { _score = 0, _bonus = (0,0), _last = Nothing, _frame = 0 }
 
 
