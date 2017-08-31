507ac32c3e43a2f80c3b62270f2acd27a7f06846
ToF <cthibaut@octo.com>
2017-08-31 20:58:16 +0200

    refactoring

--- Frames/Bowling.hs
+++ Frames/Bowling.hs
@@ -4,49 +4,37 @@ import Data.Maybe
 
 type Throw = Int
 type Score = Int
-data Frame = Frame Int Result 
+data Frame = Fr Int FrameType
     deriving (Eq, Show)
+data FrameType = Half Throw | Normal | Spare | Strike | BonusThrow
+    deriving (Eq, Show)
+    
 type Index = Int
 
-data Result = Normal | Half Int | Spare | Strike
-    deriving (Eq,Show)
-
 score :: [Throw] -> Score
-score ts = collect ts $ marks $ frames $ results ts
-    where 
-    collect :: [Throw] -> [Index] -> Score
-    collect ts ps = sum $ map throwAt ps
-        where
-        game :: [(Int,Throw)]
-        game = zip [0..] ts
-
-        throwAt :: Index -> Throw
-        throwAt = fromJust . flip lookup game
-
-results :: [Throw] -> [Result]
-results = tail . scanl result Normal 
-
-frames :: [Result] -> [Frame]
-frames = tail . scanl nextFrame (Frame (-1) Normal)
+score ts = collect $ indices $ frames $ ts
     where
-    nextFrame (Frame 10 _       ) r   = Frame 10 r
-    nextFrame (Frame n  (Half _)) r   = Frame n  r
-    nextFrame (Frame n   _      ) r   = Frame (succ n) r 
 
-marks :: [Frame] -> [Index]
-marks fs = concat $ zipWith mark fs [0..]
-    where
-    mark :: Frame -> Index -> [Index]
-    mark (Frame n     _)  _ | n == 10 = []
-    mark (Frame _ Spare)  i           = [i, i+1]
-    mark (Frame _ Strike) i           = [i, i+1, i+2]
-    mark _                i           = [i]
-    
-result :: Result -> Throw -> Result
-result (Half n) t | n + t == 10 = Spare
-                  | otherwise   = Normal
-result _        10              = Strike
-result _        t               = Half t
+    collect :: [Index] -> Score
+    collect = sum . map (ts!!) 
 
+    frames :: [Throw] -> [Frame]
+    frames = tail . scanl next (Fr (-1) Normal)
 
+    next :: Frame -> Throw -> Frame
+    next (Fr n (Half p')) p | p + p' == 10 = Fr n Spare
+    next (Fr n (Half _ )) _                = Fr n Normal
+    next (Fr 9 _) _  = Fr 9 BonusThrow
+    next (Fr n _) 10 = Fr (succ n) Strike
+    next (Fr n _) p  = Fr (succ n) (Half p) 
+                               
 
+    indices :: [Frame] -> [Index]
+    indices fs = concat $ zipWith count [0..] fs
+
+    count :: Index -> Frame -> [Index]
+    count _ (Fr _ BonusThrow) = []
+    count i (Fr _ (Half _))   = [i]
+    count i (Fr _ Normal)     = [i]
+    count i (Fr _ Spare)      = [i,succ i]
+    count i (Fr _ Strike)     = [i,succ i, succ $ succ i]
