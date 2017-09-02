0ecd57596ae0b102aeff804b6ef86420a5eb539b
ToF <cthibaut@octo.com>
2017-09-02 14:13:15 +0200

    recursive version

--- WithQC/Recursive/Bowling.hs
+++ WithQC/Recursive/Bowling.hs
@@ -4,6 +4,15 @@ type Throw = Int
 type Score = Int
 
 score :: [Throw] -> Score
-score ts = sum ts
+score = score' 0
+    where
+    score' :: Int -> [Throw] -> Score
+    score' f [] = 0
+    score' 10 _ = 0
+    score' f (t:u:v:ts) | t == 10   = t+u+v + score' (succ f) (u:v:ts)
+    score' f (t:u:v:ts) | t+u == 10 = t+u+v + score' (succ f) (v:ts)
+    score' f (t:u:ts)               = t+u   + score' (succ f) (ts)
+    score' f (t:ts)                 = t + score' f ts
+    
 
 
